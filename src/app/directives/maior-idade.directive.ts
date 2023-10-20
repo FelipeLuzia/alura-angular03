98704835038f6a99111cc059366997c048e5973e
Felipe Luzia <felipe.luzia@hotmail.com>
2023-10-20 15:31:28 -0300

    04 03 Diretiva de validação

--- src/app/directives/maior-idade.directive.ts
+++ src/app/directives/maior-idade.directive.ts
@@ -1,10 +1,33 @@
-import { Directive } from '@angular/core';
+import { Directive } from "@angular/core";
+import {
+  AbstractControl,
+  NG_VALIDATORS,
+  ValidationErrors,
+  Validator,
+} from "@angular/forms";
 
 @Directive({
-  selector: '[appMaiorIdade]'
+  selector: "[maiorIdadeValidator]",
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: MaiorIdadeDirective,
+      multi: true,
+    },
+  ],
 })
-export class MaiorIdadeDirective {
+export class MaiorIdadeDirective implements Validator {
+  constructor() {}
 
-  constructor() { }
+  validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    const dataNascimento = control.value;
+    const anoNascimento = new Date(dataNascimento).getFullYear();
+    const anoNascMais18 = anoNascimento + 18;
 
+    const anoAtual = new Date().getFullYear();
+
+    const isMaior = anoNascMais18 <= anoAtual;
+
+    return isMaior ? null : { maioridade: true };
+  }
 }
