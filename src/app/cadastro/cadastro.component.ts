100e772e7cad36c1bfaff26c1743f04821900777
Felipe Luzia <felipe.luzia@hotmail.com>
2023-10-20 19:11:44 -0300

    05 06 NG_ASYNC_VALIDATORS: validando CEP

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -30,8 +30,8 @@ export class CadastroComponent implements OnInit {
 
     if (cep !== "") {
       this.consultaCepService.getConsultaCep(cep).subscribe((resultado) => {
-        console.log(resultado);
         this.populandoEndereco(resultado, f);
+        console.log(resultado);
       });
     }
   }
